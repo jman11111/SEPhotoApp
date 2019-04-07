d6f916bc54efe84b44baa27207d9ce10cca37943
jbroncato <jbroncato@luc.edu>
2019-04-07 17:36:53 -0500

    Searchbar works

--- client/apple/src/index.js
+++ client/apple/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import Search from './components/Search';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
