8d3519cde47672cae5f4ff4b45177ce40870a842
jbroncato <jbroncato@luc.edu>
2019-04-12 15:06:31 -0500

    small changes

--- client/apple/src/components/Linklist.js
+++ client/apple/src/components/Linklist.js
@@ -1,19 +1,35 @@
 import React, { Component } from 'react'
 import Link from './Links'
+import { Query } from 'react-apollo'
+import gql from 'graphql-tag'
+
+const FEED_QUERY = gql`
+  {
+    findCandy(cname: "Pez"){
+        Candy{
+            links
+        }
+    }
+  }
+`
 
 class LinkList extends Component {
   render() {
-    const linksToRender = [
-      {
-        url: 'https://www.prismagraphql.com',
-      },
-      {
-        url: 'https://www.apollographql.com/docs/react/',
-      },
-    ]
-
     return (
-      <div>{linksToRender.map(link => <Link url={link.url} />)}</div>
+        <Query query={FEED_QUERY}>
+            {({ loading, error, data }) => {
+                if (loading) return <div>Fetching</div>
+                if (error) return <div>Error</div>
+    
+            const linksToRender = data.Candy
+
+            return (
+                <div>
+                    {linksToRender.map(link => <Link url={link.links[0]} />)}
+                </div>
+            )
+            }}
+        </Query>
     )
   }
 }
