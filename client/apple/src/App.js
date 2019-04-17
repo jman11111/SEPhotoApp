4572997fb9d7de566e5a831cd2ecd0ce8b1a5218
jlputter <jlputter17@gmail.com>
2019-04-17 10:01:24 -0500

    NavBar created, need to move search bar and button in search-button branch

--- client/apple/src/App.js
+++ client/apple/src/App.js
@@ -3,11 +3,13 @@ import logo from "./logo.svg";
 import "./App.css";
 import Search from "./components/Search";
 import Button from "./components/button";
+import NavBar from "./components/navbar";
 
 class App extends Component {
   render() {
     return (
       <React.Fragment>
+        <NavBar />
         <Search />
       </React.Fragment>
     );
