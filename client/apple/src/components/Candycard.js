0fd8797de493555f3fdd9ca33e2854b1eb9eae36
JackNarowski <jacknarowski@gmail.com>
2019-04-18 17:00:41 -0500

    Button to link to candy works

--- client/apple/src/components/Candycard.js
+++ client/apple/src/components/Candycard.js
@@ -4,6 +4,8 @@ import { withStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card'; //yarn add @material-ui/core
 import CardHeader from '@material-ui/core/CardHeader';
 import CardMedia from '@material-ui/core/CardMedia';
+import CardActions from '@material-ui/core/CardActions';
+import Button from '@material-ui/core/Button';
 
 const styles = theme => ({
     card: {
@@ -14,6 +16,8 @@ const styles = theme => ({
         paddingTop: '56.25%', // 16:9
     },
 });
+
+
 
 class Candycard extends React.Component {
     render(){
@@ -30,6 +34,12 @@ class Candycard extends React.Component {
                     className={classes.media}
                     image="https://images.pexels.com/photos/67636/rose-blue-flower-rose-blooms-67636.jpeg"
                 />
+
+                <CardActions>
+                    <Button href="https://www.google.com" size="small" color="primary">
+                        Buy
+                    </Button>
+                </CardActions>
 
             </Card>
         );
