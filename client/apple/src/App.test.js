932285c8a28ab28ea176bd044c7dbc5e8774be63
JackNarowski <jacknarowski@gmail.com>
2019-04-26 19:30:46 -0500

    Added front end testing using JEST

--- client/apple/src/App.test.js
+++ client/apple/src/App.test.js
@@ -1,9 +1,42 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-
-it('renders without crashing', () => {
+import NavBar from "./components/navbar";
+import Search from "./components/Search";
+import Tagform from "./components/Tagform";
+import Candycard from "./components/Candycard";
+import Button from "./components/button";
+it('App renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(<React.Fragment/>, div);
   ReactDOM.unmountComponentAtNode(div);
+});
+describe('Seperate Components', () => {
+  it('NavBar Component loads', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<NavBar/>, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+  it('Candycard Component loads', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<Candycard/>, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+  it('Button Component loads', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<Button/>, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+});
+describe('SHOULD FAIL w/o database', () => {
+  it('Search Combined Component loads', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<Search/>, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+  it('TagForm Combined Component loads', () => {
+    const form = document.createElement('form');
+    ReactDOM.render(<Tagform/>, form);
+    ReactDOM.unmountComponentAtNode(form);
+  });
 });
